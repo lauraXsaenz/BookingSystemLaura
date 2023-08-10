aeaaf8e143aaee8dc4547d8dc8d0059b4ba9b3c7
Laura <ximesaenz@gmail.com>
2023-08-09 19:31:14 -0700

    Initial commit

---
+++ src/main/java/com/example/bookingsystemlaura/BookingSystemLauraApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookingsystemlaura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSystemLauraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSystemLauraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookingsystemlaura/BookingSystemLauraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookingsystemlaura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemLauraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
